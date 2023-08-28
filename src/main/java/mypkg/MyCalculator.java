296a4806d0d567f287a22967dc71424ac7d0e903
kamdishashwat-17 <kamdishashwat17@gmail.com>
2023-08-28 10:17:55 +0530

    mul fun version

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -11,12 +11,16 @@ public class MyCalculator {
 	{
 		return(a-b);
 	}
+	public int mul(int a, int b)
+	{
+		return(a*b);
+	}
 	public static void main(String[] args) {
 		
 		MyCalculator calc = new MyCalculator ();
 		System.out.println("Sum is "+calc.sum(20, 10));
 		System.out.println("Diff is "+calc.diff(20, 10));
-		
+		System.out.println("mul is "+calc.mul(20, 10));
 	
 	}
 
